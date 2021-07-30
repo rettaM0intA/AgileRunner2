6bde4bfd22d1f4be93b2a8e93dc2c9f7616e4a13
Nick <20Nick@users.noreply.github.com>
2021-07-29 23:17:06 -0400

    Fixed warnings
    Fixed warnings in the project.

--- src/main/java/frc/robot/commands/ChassisDefaultCommand.java
+++ src/main/java/frc/robot/commands/ChassisDefaultCommand.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.Robot;
 import frc.robot.RobotContainer;
 
 public class ChassisDefaultCommand extends CommandBase {
